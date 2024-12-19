e305ac143cc9619fc8bf7a8bcde8ea665ebbed27
Kevin <kevinglennon01@gmail.com>
2024-12-19 00:35:12 +0000

    Changing styles

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -6,7 +6,7 @@ import SignupForm from "../components/Signup";
 const AuthPage = () => {
   const [isSignup, setIsSignup] = useState(false);
   const [token, setToken] = useState("");
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
 
   const toggleForm = () => {
     setIsSignup(!isSignup);
@@ -15,28 +15,30 @@ const AuthPage = () => {
   const handleLogin = (token) => {
     setToken(token);
     localStorage.setItem("token", token);
-    navigate("/rooms"); // Redirect after successful login
+    navigate("/rooms");
   };
 
   const handleSignup = (token) => {
     setToken(token);
     localStorage.setItem("token", token);
-    navigate("/rooms"); // Redirect after successful signup
+    navigate("/rooms");
   };
 
   return (
-    <div>
-      <h1>{isSignup ? "Create an Account" : "Login"}</h1>
-      {isSignup ? (
-        <SignupForm onSignup={handleSignup} />
-      ) : (
-        <LoginForm onLogin={handleLogin} />
-      )}
-      <button onClick={toggleForm}>
-        {isSignup
-          ? "Already have an account? Login"
-          : "Don't have an account? Sign up"}
-      </button>
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <h1>{isSignup ? "Create an Account" : "Login"}</h1>
+        {isSignup ? (
+          <SignupForm onSignup={handleSignup} />
+        ) : (
+          <LoginForm onLogin={handleLogin} />
+        )}
+        <button className="toggle-btn" onClick={toggleForm}>
+          {isSignup
+            ? "Already have an account? Login"
+            : "Don't have an account? Sign up"}
+        </button>
+      </div>
     </div>
   );
 };
